387ee6dc6705a5eabbdf70dc7dcf9fed6e9806a5
dev-ops-geo <ohururobert@gmail.com>
2024-10-21 15:13:04 +0200

    Removing unused files

--- src/store/actions.tsx
+++
@@ -1,7 +0,0 @@
-export const fetchCountry = (id: number) => {
-    return { type: "FETCH_COUNTRY", payload: id };
-};
-  
-export const fetchRegion = (id: number) => {
-    return { type: "FETCH_REGION", payload: id };
-};
